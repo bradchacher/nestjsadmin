ca6d8e2e66e5492a1bd2b0611870917b3fdb554e
bradchacher <bradchacher@gmail.com>
2020-01-03 04:58:24 +0300

    added task dto and crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -9,6 +10,20 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[]{
     return this.tasksService.getAllTasks()
+  }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id:string): void {
+    this.tasksService.deleteTask(id);
   }
 }
